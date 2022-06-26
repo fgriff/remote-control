1309f20a2183a24d9052bb26ba328e5eb0fe4a29
fgriff <anglirum@gmail.com>
2022-06-26 15:10:58 +0300

    feat: implement mouse navigation

--- src/services/log.service.ts
+++ src/services/log.service.ts
@@ -2,4 +2,9 @@ const printSuccess = (commandName: string) => {
   console.log(`${commandName} successfully done`);
 };
 
-export { printSuccess };
+const printMouseNav = (commandName: string, coordOne: number, coordTwo?: number) => {
+  const result = coordTwo ? `${commandName} ${coordOne} ${coordTwo}` : `${commandName} ${coordOne}`;
+  console.log(result);
+};
+
+export { printMouseNav, printSuccess };
--- src/utils/dispatch.ts
+++ src/utils/dispatch.ts
@@ -1,9 +1,10 @@
+import { moveMouse } from 'robotjs';
 import { WebSocket } from 'ws';
 import { drawCircle } from '../drawing/drawCircle';
 import { drawRectangle } from '../drawing/drawRectangle';
 import { drawSquare } from '../drawing/drawSquare';
 import { printScreen } from '../printScreen/printScreen';
-import { printSuccess } from '../services/log.service';
+import { printMouseNav, printSuccess } from '../services/log.service';
 
 const dispatch = async (
   command: string,
@@ -14,6 +15,39 @@ const dispatch = async (
   ws: WebSocket
 ) => {
   switch (command) {
+    case 'mouse_up':
+      const yUp = y - value;
+      ws.send('mouse_up');
+      moveMouse(x, yUp);
+      printMouseNav(command, yUp);
+      break;
+
+    case 'mouse_down':
+      const yDown = y + value;
+      ws.send('mouse_down');
+      moveMouse(x, yDown);
+      printMouseNav(command, yDown);
+      break;
+
+    case 'mouse_left':
+      const xLeft = x - value;
+      ws.send('mouse_left');
+      moveMouse(xLeft, y);
+      printMouseNav(command, xLeft);
+      break;
+
+    case 'mouse_right':
+      const xRight = x + value;
+      ws.send('mouse_right');
+      moveMouse(xRight, y);
+      printMouseNav(command, xRight);
+      break;
+
+    case 'mouse_position':
+      ws.send(`mouse_position ${x} ${y}`);
+      printMouseNav(command, x, y);
+      break;
+
     case 'draw_circle':
       drawCircle(x, y, value, 0.03);
       printSuccess(command);
