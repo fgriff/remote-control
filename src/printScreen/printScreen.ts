6e9c438b010c5f350ccc83aa272983204ce3b24d
fgriff <anglirum@gmail.com>
2022-06-26 14:08:10 +0300

    fix: correct screenshot color

--- src/printScreen/printScreen.ts
+++ src/printScreen/printScreen.ts
@@ -2,13 +2,20 @@ import Jimp from 'jimp';
 import { screen } from 'robotjs';
 
 const printScreen = async (x: number, y: number, sideSize: number) => {
-  const bitmapImg = screen.capture(x, y, sideSize, sideSize);
+  const centerX = x - sideSize / 2;
+  const centerY = y - sideSize / 2;
 
-  const jimp = new Jimp({
-    data: bitmapImg.image,
-    width: bitmapImg.width,
-    height: bitmapImg.height,
-  });
+  const bitmapImg = screen.capture(centerX, centerY, sideSize, sideSize);
+
+  const jimp = new Jimp(sideSize, sideSize);
+
+  for (let x = 0; x < sideSize; x++) {
+    for (let y = 0; y < sideSize; y++) {
+      const hexColor = bitmapImg.colorAt(x, y);
+      const color = parseInt(`${hexColor}ff`, 16);
+      jimp.setPixelColor(color, x, y);
+    }
+  }
 
   const base64 = await jimp.getBase64Async(Jimp.MIME_PNG);
 
